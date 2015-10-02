cf4ce6011cb9c3033643f9485dca06db2c69cbe5
br0vey <brovey@ya.ru>
2015-10-02 16:06:35 +0400

    change ddu

--- Technosphere/2s.sh
+++ Technosphere/2s.sh
@@ -1,1 +1,1 @@
- cat 2.txt | perl -F: -nlae '$str++;$stb=0;foreach my $q (@F){$stb++; print "stobets:$stb\n stroka:$str\n", if $q>10;}'
+ cat 2.txt | perl -F: -nlae '$str++;$stb=0;foreach my $elem (@F){$stb++; print "stobets:$stb\n stroka:$str\n", if $elem>10;}'
