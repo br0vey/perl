753bf9f299ebb132b159ae7d428fa1c6f0106d88
br0vey <brovey@ya.ru>
2015-10-11 13:19:08 +0400

    rpn calc done

--- Technosphere/calc.pl
+++ Technosphere/calc.pl
@@ -15,6 +15,7 @@ my $var2;
 my $var3;
 my @mm;
 my @rpn;
+my @rpnst;
 my @stack;
 my @scobki;
 my $znak;
@@ -49,8 +50,8 @@ my %stkoppr;
 
 $var1 = "- 16 + 2 * 0.3e+2 - .5 ^ ( 2 - 3 )";
 #$var1 = "2 + 3 * 4";
-%inoppr = ('(','99', ')', 1, '+', '4', '-', '4', '*', '9', '**', '15');  
-%stkoppr = ('(' , '2','+', '5', '-', '5', '*', '11', '**', '14');
+%inoppr = ('(','99', ')', 1, '+', '4', '-', '4', '*', '9','/', '9', '**', '15');  
+%stkoppr = ('(' , '2','+', '5', '-', '5', '*', '5', '/', '11', '**', '14');
 
 @m = split(/ /, $var1);
 foreach my $elem ( @m ) {
@@ -79,11 +80,7 @@ if ($m[0] eq "-"){
 foreach $elem (@mm) {
 print "00_stack:@stack\n";
 print "00_elem:$elem\n";
-#$l++;
-#print "$_\n";      
-     # $step = 0 if ( $elem eq ")" );
-#      if ( $elem ~~ ["+", "-", "*", "**"]) {
-       if ( $elem ~~ [keys(%inoppr)]) {
+if ( $elem ~~ [keys(%inoppr)]) {
      $token_count = 1;
      while ( @stack ) {
          $znak = $stack[$#stack];
@@ -91,19 +88,7 @@ print "00_elem:$elem\n";
          print "wznak:$znak\n";
          print "w_>:$stkoppr{$znak}>$inoppr{$elem}\n";
          
-         $token_count = 7 if ($elem eq ")");
          
-#           if ( $token_count == 7 ) {
-#            if ($stkoppr{$znak} < $inoppr{$elem}) {
-#             print "ww_>:$stkoppr{$znak}<$inoppr{$elem}\n";
-#             push ( @rpn, $znak);
-#             print "ww_rpn:@rpn\n";
-#             pop @stack ;
-#             $token_count = 7;
-#           }
-
-#          $token_count = 1 
-#         }
           if ($stkoppr{$znak} > $inoppr{$elem}) {
            print "w_>:$stkoppr{$znak}>$inoppr{$elem}\n";   
            push ( @rpn, $znak);
@@ -113,25 +98,6 @@ print "00_elem:$elem\n";
          }
          else { last }
 }
-#       print "stkoppr{znak}:$inoppr{'$elem'}\n";
-#        foreach $znak ( @stack ) {               
-#               if ( $stkoppr{$znak} > $inoppr{$elem} ){
-#                print "11_stack:@stack\n";
-#                print "11_elem:$elem\n"; 
-#                print "11_>:$stkoppr{$znak}>$inoppr{$elem}\n";                 
-#                # print "11_elem:$elem\n";
-#                 $y=$znak;
-#                 $znak=" ";
-#                 $token_count = 7;
-#                 push ( @rpn, $y);
-#                 print "11_rpn:@rpn\n";            
-
-          #print "rabotaet_elem:$elem\n";
- #              }        
-#       }
-#      if ($elem eq "(" || ")"){   
- #     $elem= " ";
-  #    }
       
      push ( @stack, $elem );
 
@@ -151,42 +117,45 @@ print "00_elem:$elem\n";
       }
      # $step = 1 if ( $elem eq "(" );
 }
-#print "---$plus---\n";
-#$plus = 1 if ( $elem eq "*" ); 
-#print "---$plus---\n";
-#$plus = 0 if ( $_ ne "+" );
-#       if ( $plus == 1 ) {
-#print "---$elem---\n";
-#       $x=$mm[$l+2];
-#       $y=$mm[$l-2];
-#       $z = $x * $y;
-#      print "-y--$y---\n";       
-#       print "-x--$x---\n";
-#      print "-z--$z---\n";  
-#       }
-#$plus = 0 if ( $_ ne "*" );
-#} 
+###########################RPN!!!111
+###########################RPN vichisleniya!!!111
 
-#foreach $elem ( @mm ) {
-#     print "+++$elem++\n";
-#     if ( $elem ~~ ["+", "-", "*", "**"] ){
-#     push @stack, $elem ;
-#      $i++;
-#      if ( $elem eq "*" ){ 
-#      $tmpv1 = 1;
-#      $tmpv=$elem; 
-#      }
-#     }
-#     else {
-#     push @rpl, $elem; 
-#        if ($tmpv1 == 1){
-#        push @rpl, $tmpv;
-#        push @rpl, $stack[$i - 2];
-#        $tmpv1 = 0;
-#        }
-     
-#     }
-#}
+foreach $elem (@rpn) {
+print "rpnst:@rpnst\n";
+print "03_elem:$elem\n";
+       if ( $elem ~~ [keys(%inoppr)]) {
+      push (@rpnst, pop(@rpnst) + pop(@rpnst) )   if ($elem eq "+"); 
+      if ($elem eq "*") {
+        $x = pop(@rpnst);
+         push (@rpnst, pop(@rpnst) * $x );
+      }
+      if ($elem eq "-") {
+        $x = pop(@rpnst); 
+         push (@rpnst, pop(@rpnst) - $x );
+      }                          
+      if ($elem eq "**") {
+        $x = pop(@rpnst);
+         push (@rpnst, pop(@rpnst) ** $x );
+      }
+      if ($elem eq "/") {
+        $x = pop(@rpnst);
+        push (@rpnst, pop(@rpnst) / $x );
+      }
+
+      print "rpnst:@rpnst\n";
+       }
+       else {
+
+           print "33_rpn:@rpnst\n";
+           $token_count = 0;
+           push ( @rpnst, $elem );
+           print "33_rpn:@rpnst\n";
+      }
+
+
+}
+
+
 
 print "stack:@stack\n";
 print "m:@m\n";
@@ -196,3 +165,4 @@ print "rpn:@rpn\n";
 print "scobki:@scobki\n";
 $var3=eval ($var2);
 print "---$var3---\n";
+print "rpnst:@rpnst\n";
