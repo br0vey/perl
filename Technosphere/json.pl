3ae08a9fabf6c08c15f02c61e7acc9dd59e6d9ac
br0vey <brovey@ya.ru>
2015-10-24 14:46:59 +0400

    json

--- Technosphere/json.pl
+++ Technosphere/json.pl
@@ -6,42 +6,36 @@
      decode_jsonl
      encode_jsonl
      );
-my $string;
-my $array_ref;
-my %hash1;
-my %hash2;
-my %hash3;
-my @data;
-my @days;
+
 
 #@data=(\%hash1, \%hash2, \%hash3);
 
 #@data=({ k => "test},{test"}, \%hash1, { k => "test},{test"},\%hash2);
-@data=({ k => "test},{test"}, { k => "test},{test"},{ k => "test},{test"});
-#@data=( \%hash1, \%hash2);
-@days=(3,4,6,8,9,5);
-%hash1 = (
+
+#my @data=({k => "},{" }, [1,2,3]);
+my @days=(3,4,6,8,9,5);
+my %hash1 = (
        'month' => 3,
        'year' => 2017,
        'days' => \@days,
 );
-%hash2 = (
+my %hash2 = (
        'month' => 5,
        'year' => 2014,
        'days' => 654,
 );
 
-%hash3 = (
+my %hash3 = (
        'month' => 8,
        'year' => 2007,
        'days' => 56,
 );
+my @data=({ k => "test},{test"}, { y => "test},{test"},[1,2,3],{ y => "test},{test"},[8,6,4],{ y => "test},{test"},[5,6,7], { y => "test},{test"},\%hash1,[11,56,67],\%hash1, { w => "test},{test"});
 
 
-$array_ref=\@data;
-$string = encode_jsonl($array_ref);
+my $string = encode_jsonl(\@data);
 print "string:$string\n";
 
-$array_ref = decode_jsonl($string);
-print  Dumper($array_ref); 
+my $arrayref = decode_jsonl($string);
+print  Dumper($arrayref); 
 
