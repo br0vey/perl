2016d894c498871162e5b0abec8158a2d0744a58
br0vey <brovey@ya.ru>
2015-10-24 22:18:00 +0400

    json work again

--- Technosphere/jsonl.pm
+++ Technosphere/jsonl.pm
@@ -6,7 +6,7 @@
  use JSON;
 our @EXPORT;
 @EXPORT = qw(decode_jsonl encode_jsonl);
-
+our $tr1 = 0;
 sub decode_jsonl {
    my $tmpp = shift;
    $tmpp =~ s/\n/,/g;
@@ -19,10 +19,24 @@ sub encode_jsonl{
    my @mm = @$elem;
    my $strout = "";
    my $str = "[";
+   my $tr1 = 0;
    foreach my $em (@mm){
+   #      $tr1 = 0; 
           $strout = $str;
-          $str = $strout . encode_json($em) . "\n" ;
-}
+          $str = encode_json($em);
+#          print "str1:$str\n";
+ #        $str = qq($str);
+#          $str =~ s/\\\\n/\\n/g if $tr1 == 0; 
+  #        print "str4:$str\n";
+          if ($str =~ s/\\\\n/\\n/g){
+            $tr1 = 1;
+          }
+          $str =~ s/(?<=\w)\\n/\\\\n/g if $tr1 == 0;               
+ #         print "str2:$str\n";
+    #      $str =~ s/\\\\n/\\n/g if $tr1 = 1;
+  #        print "str7:$str\n";
+          $str = $strout . $str . "\n" ;
+   }
 chop $str;
 $str = $str . "]"; 
 return $str;
