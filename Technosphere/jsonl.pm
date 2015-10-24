5b94d3c80c685c7623e5a814baed0780145114db
br0vey <brovey@ya.ru>
2015-10-24 14:47:22 +0400

    json

--- Technosphere/jsonl.pm
+++ Technosphere/jsonl.pm
@@ -7,27 +7,40 @@
 our @EXPORT;
 @EXPORT = qw(decode_jsonl encode_jsonl);
 
-
 sub decode_jsonl {
-   my $hashout;
-   my $jsontextin; 
-   my @tmparr;
-   my $elem;
-   @tmparr = @_;
-   $tmparr[$#tmparr] =~ s/\n/,/g;
-   ($jsontextin) = @tmparr;
-   $hashout = decode_json($jsontextin);
+#@_;
+my $tmpp = shift;
+#   my tmparr = @_;
+   $tmpp =~ s/\n/,/g;
+
+#   ($jsontextin) = @tmp;
+ my  $hashout = decode_json($tmpp);
    return $hashout;
 }
 
 sub encode_jsonl{
-   my $elem;
-   my $perl_scalarin;
-   my $jsontextout;;
-   ($perl_scalarin) = @_;
-   $jsontextout = JSON->new->utf8->encode($perl_scalarin);
-   #print "jsontext:$jsontext\n";
-   $jsontextout =~ s/,{"/\n{"/g;
+# my $elem;
+my ($ar) = @_;
+print "ar:@$ar\n";
+my ($arr) =  @$ar;
+print "arr:$arr\n";
+my @m;
+#@m = shift @$ar;
+#print "m:@m\n";
+my   ($perl_scalarin) = @_;
+#print "m:@{$perl_scalarin}\n";
+#my   $jsontextout = JSON->new->utf8->encode(@$ar);
+my   $jsontextout = encode_json($perl_scalarin);
+#my   $jsontextout = encode_json(@$ar);
+#$my   $jsontextout = JSON->new->utf8->encode(@$ar);
+   print "jsontextout:$jsontextout\n";
+   $jsontextout =~ s/(?<="}),(?=[[])/\n/g;
+   $jsontextout =~ s/(?<=[]]),(?=[[])/\n/g;
+   $jsontextout =~ s/(?<=[]]),(?={")/\n/g;
+   $jsontextout =~ s/(?<="}),(?={")/\n/g;
+   $jsontextout =~ s/(?<=}),(?={")/\n/g;
+   $jsontextout =~ s/(?<="}),(?={)/\n/g;  
+   $jsontextout =~ s/(?<=}),(?=[[])/\n/g;
    return $jsontextout;
 }
 1;
