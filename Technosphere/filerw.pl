75c7aba4414e0fc87ad5e2ffd5fed03611e201bb
br0vey <brovey@ya.ru>
2015-11-18 11:45:05 +0400

    filerw

--- Technosphere/filerw.pl
+++ Technosphere/filerw.pl
@@ -1,20 +1,19 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-#my $fname = 'file.txt';
-#my $fh;
-open (my $fhw, ">>", "file.txt");
+open( my $fhw, ">>", "file.txt" );
 my @data = <STDIN>;
 my $lnum = 1;
-for my $ent (@data){
-print $fhw "$lnum $ent";
-$lnum++;
+for my $ent (@data) {
+    print $fhw "$lnum $ent";
+    $lnum++;
 }
 close($fhw);
-open (my $fhr, "<", "file.txt");
-while (<$fhr>){
-$_ =~ s/\d+\s//;
-print "line:$_\n";
+open( my $fhr, "<", "file.txt" );
+while (<$fhr>) {
+    $_ =~ s/\d+\s//;
+    print "line:$_\n";
 }
-#print $fh "data:@data";
 close($fhr);
+
+
